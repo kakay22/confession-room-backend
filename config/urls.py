2f5c97310f7d7532e12d3babdf60a2af54529967
kyle calites <kylecalites3@gmail.com>
2026-09-11 10:45:39 +0800

    fixed url route

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
 
+def home(request):
+    return JsonResponse({
+        "message": "Confession Room API is running.",
+        "status": "online",
+    })
+
+
 urlpatterns = [
+    path("", home, name="home"),
+
     path("admin/", admin.site.urls),
 
     path("api/auth/", include("accounts.urls")),
@@ -12,7 +22,6 @@ urlpatterns = [
 ]
 
 
-# Serve media during local development only.
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
